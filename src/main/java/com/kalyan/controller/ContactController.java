6efdd5bbb6a33107c55f1d8317e82666bed0f4b8
thalarikalyan <thalarikalyaninfo@gmail.com>
2024-02-27 17:08:12 -0500

    Updated the changes

--- src/main/java/com/kalyan/controller/ContactController.java
+++ src/main/java/com/kalyan/controller/ContactController.java
@@ -5,19 +5,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.kalyan.entity.Contact;
 import com.kalyan.exception.NoContactFoundException;
 import com.kalyan.service.ContactService;
 
-@Controller
+@RestController
 @RequestMapping("/ContactDetailsWar")
 public class ContactController {
 
